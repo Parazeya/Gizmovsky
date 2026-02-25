28f8720bd61f7b02223af6e9fe0a52669fac94ca
Parazeya <zezarUS@bk.ru>
2026-02-25 15:25:51 +0300

    JSDoc fix

--- rollup.config.js
+++ rollup.config.js
@@ -5,24 +5,52 @@ import { terser } from 'rollup-plugin-terser';
 
 const name = 'gizmoSdk';
 
-export default {
-  input: 'src/index.js',
-  external: ['axios', 'event-source-plus', 'eventsource'],
-  output: [
-    {
-      file: 'dist/gizmo-sdk.cjs.js',
-      format: 'cjs',
-      exports: 'named'
-    },
-    {
-      file: 'dist/gizmo-sdk.esm.js',
-      format: 'es'
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    json(),
-    terser()
-  ]
-};
+export default [
+  // Development build (with JSDoc comments, not minified)
+  {
+    input: 'src/index.js',
+    external: ['axios', 'event-source-plus', 'eventsource'],
+    output: [
+      {
+        file: 'dist/gizmo-sdk.cjs.js',
+        format: 'cjs',
+        exports: 'named'
+      },
+      {
+        file: 'dist/gizmo-sdk.esm.js',
+        format: 'es'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      json()
+    ]
+  },
+  // Production build (minified)
+  {
+    input: 'src/index.js',
+    external: ['axios', 'event-source-plus', 'eventsource'],
+    output: [
+      {
+        file: 'dist/gizmo-sdk.cjs.min.js',
+        format: 'cjs',
+        exports: 'named'
+      },
+      {
+        file: 'dist/gizmo-sdk.esm.min.js',
+        format: 'es'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      json(),
+      terser({
+        output: {
+          comments: false
+        }
+      })
+    ]
+  }
+];
